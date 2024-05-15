d72c58bbee15e828ac7af1194681693e540bd574
NatalieVanDelm <natalie.vandelm@hotmail.com>
2024-05-15 12:41:37 +1000

    movie crud api, needs fixes

--- main.go
+++ main.go
@@ -1,10 +1,80 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
+
+type Movie struct {
+	ID       string    `json: "id"`
+	Isbn     string    `json: "isbn"`
+	Title    string    `json: "title"`
+	Director *Director `json: "director"`
+}
+
+type Director struct {
+	Firstname string `json: "firstname"`
+	Lastname  string `json: "lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := r.URL.Query().Get("id")
+	for _, item := range movies {
+		if item.ID == id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := r.URL.Query().Get("id")
+	for index, item := range movies {
+		if item.ID == id {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := r.URL.Query().Get("id")
+	for index, item := range movies {
+		if item.ID == id {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = id
+			movies = append(movies[:index], movie)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
@@ -28,14 +98,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// webserver:
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
+	// crud api:
+	movies = append(movies, Movie{ID: "1", Isbn: "48227", Title: "Movie1", Director: &Director{Firstname: "John", Lastname: "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "45455", Title: "Movie2", Director: &Director{Firstname: "Jane", Lastname: "Smith"}})
+	r := http.NewServeMux()
+	r.HandleFunc("/GET/movies", getMovies)
+	r.HandleFunc("/GET/movies/{id}", getMovie)
+	r.HandleFunc("/POST/movies", createMovie)
+	r.HandleFunc("/PUT/movies/{id}", updateMovie)
+	r.HandleFunc("/DELETE/movies/{id}", deleteMovie)
+
 	fmt.Printf("Starting server at port 8080\n")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
